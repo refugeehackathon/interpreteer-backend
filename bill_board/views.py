5263ae0387e4c398a4734322affdc6aaf2abe156
illing <sebastian@wgcast.de>
2015-10-25 00:40:17 +0200

    create requests

--- bill_board/views.py
+++ bill_board/views.py
@@ -7,3 +7,10 @@ class RequestsViewset(ModelViewSet):
     
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
+
+    def perform_create(self, serializer):
+        '''
+        Set User as creator
+        '''
+        instance = serializer.save(user=self.request.user)
+        return instance
