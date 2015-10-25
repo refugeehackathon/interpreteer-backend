ffb0432e6da0807ab1b8e2289b6dc688537d3765
Daniel Hoffmann <daniel.hoffmann@student.hpi.uni-potsdam.de>
2015-10-25 11:39:42 +0100

    remove static files settings for now

--- interpreteer/settings/base.py
+++ interpreteer/settings/base.py
@@ -350,6 +350,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
